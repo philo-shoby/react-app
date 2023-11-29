b18e56e2dfddab2303fd134750c7d60da1c3d80e
philo-shoby <shoby.philo@gmail.com>
2023-11-30 02:13:07 +0530

    Basic skeleton of a Food Delivery App

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 Namaste React !!
 
-This repository contains React  
+This repository contains React code for Food Delivery App!
+
+Parcel
+- bundler used in this app
+- provides local server
+- Hot Module Replacement - uses File Watching algorithm (written in C++)
+- parcel - cache 
--- app.js
+++ app.js
@@ -1,15 +1,70 @@
-const head = React.createElement("h1",{id: 'heading'},
-    ["Hello React!", React.createElement("div",{ id: 'parent'},
-        [React.createElement("div",{ id: 'child'},
-            [React.createElement("h1",{}, "Hello React testing1"),
-            React.createElement("h2",{}, "Hello React testing2")]
-        ),React.createElement("div",{ id: 'child2'},
-            [React.createElement("h1",{}, "Hello React testing1"),
-            React.createElement("h2",{}, "Hello React testing2")]
-        )]
-    )]
-);
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const HeaderComponent = () => {
+    return (
+        <div className="header">
+            <img src="https://assets.materialup.com/uploads/61d86780-be13-47fa-81a6-226aac22db27/preview.jpg" />
+            <div className="nav-items">
+                <ul>
+                    <li>Home</li>
+                    <li>Contact Us</li>
+                    <li>Cart</li>
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+const RestaurantComponent = () => {
+    return (
+        <div className="resto-container">
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTJ5LStl-2beQjVnrwSyEagfP0bIsG3e9IwS6Zr3ieAcg&s" />
+            <h2>Theobroma</h2>
+            <p>Desserts, Brownies, Cakes!</p>
+            <p>8.9 Stars</p>  
+        </div>
+    )
+}
+const BodyComponent = () => {
+    return (
+        <div className="body-container">
+            <div className="search">
+                <h4>Search</h4>
+            </div>
+            <RestaurantComponent />
+            <RestaurantComponent />
+            <RestaurantComponent />
+            <RestaurantComponent />
+            <RestaurantComponent />
+            <RestaurantComponent />
+            <RestaurantComponent />
+            <RestaurantComponent />
+            <RestaurantComponent />
+            <RestaurantComponent />
+        </div>
+    )
+}
+const FooterComponent = () => {
+    return (
+        <div className="footer">
+            <p>CopyRights</p>
+            <div>
+                <p>Address: Bangalore, Karnataka</p>
+            </div>
+        </div>
+    )
+}
+const AppComponent = () => {
+    return (
+        <div>
+            <HeaderComponent />
+            <BodyComponent />
+            <FooterComponent />
+        </div>
+    )
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(head);
+root.render(<AppComponent />);
